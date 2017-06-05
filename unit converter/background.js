f50bde94926b6585ab032f1c09659f40dabc9600
Tihomir Jovchevski <tihomir.jovchevski@internetbrands.com>
2017-06-05 15:32:36 +0200

    compelte code refractoring

--- unit converter/background.js
+++ unit converter/background.js
@@ -1,111 +1,105 @@
-function replaceInElement(element, find, replace) {
-    // iterate over child nodes in reverse, as replacement may increase
-    // length of child node list.
-    for (var i= element.childNodes.length; i-->0;) {
-        var child= element.childNodes[i];
-        if (child.nodeType==1) { 
-		// ELEMENT_NODE
-            var tag= child.nodeName.toLowerCase();
-            if (tag!='style' && tag!='script') 
-			// special case, don't touch CDATA elements
-                replaceInElement(child, find, replace);
-        } else if (child.nodeType==3) { 
-		// TEXT_NODE
-            replaceInText(child, find, replace);	
-        }
-    }
-};
-function replaceInText(text, find, replace) {
-    var match;
++function(){
+	var Converter = function(){
+		this.config = {
+
+		};
+
+		this.init();
+	};
 	
-    var matches= [];
+	Converter.prototype.init = function(){
+		var self = this;
+
+		self.attachevents();
+		self.getData();
+	};
+	
+	Converter.prototype.attachevents = function(){
+
+		chrome.runtime.onMessage.addListener(
+			function(request, sender,sendResponse) {
+			if (request.applyScript == "restartWithScript" )
+				location.reload();
+		});
+	};
+
+	Converter.prototype.getData = function(){
+		chrome.storage.sync.get(["usrOp","urllist"], function(items) {
+			var url = window.location.host;
+			var t = items.urllist || [];
+			if (t.indexOf(url) == -1){
+				if(items.usrOp === 'imperial'){
+					//var kmMatch= /(\d+((\.|,)\d+)?)\s*(km|Km|Kilometer|Kilometers|kilometers)\b/g;
+					var kmMatch = /((\d{1,3}([,\s.']\d{3})*|\d+)([.,]\d+)?)\s*(km|Km|Kilometer|Kilometers|kilometers)\b/g;
+					replaceInElement(document.body, kmMatch, function(match) {
+						//var link= document.createElement('a');
+						var itmReplaced = document.createElement('b');
+						//itmReplaced.style.cssText = 'color: rgb(24, 100, 88);background-color: #eee;';
+						var value = match[2].replace(/[,\s.']/, "") + (match[4] !== undefined ? match[4].replace(/[,\s.']/, ".") : ".00");
+						itmReplaced.appendChild(document.createTextNode(fromKmtoMi(value)+' Miles '));
+						return itmReplaced;
+					});
+				}else if(items.usrOp === 'metric'){
+					//var miMatch= /(\d+((\.|,)\d+)?)\s*(mil|Mil|mile|Mile|miles|Miles)\b/g;
+					var miMatch = /((\d{1,3}([,\s.']\d{3})*|\d+)([.,]\d+)?)\s*(mil|Mil|mile|Mile|miles|Miles)\b/g;
+					replaceInElement(document.body, miMatch, function(match) {
+						//var link= document.createElement('a');
+						var itmReplaced = document.createElement('b');
+						//itmReplaced.style.cssText = 'color: rgb(24, 100, 88);background-color: #eee;';	
+						var value = match[2].replace(/[,\s.']/, "") + (match[4] !== undefined ? match[4].replace(/[,\s.']/, ".") : ".00");
+						itmReplaced.appendChild(document.createTextNode(fromMilesToKm(value)+' Kilometer '));
+						return itmReplaced;
+					});
+				}
+			}
+		});
+	};
+
+	Converter.prototype.replaceInElement = function(element, find, replace) {
+	    
+	    for (var i= element.childNodes.length; i-->0;) {
+	        var child= element.childNodes[i];
+	        if (child.nodeType==1) { 
+			// ELEMENT_NODE
+	            var tag= child.nodeName.toLowerCase();
+	            if (tag!='style' && tag!='script') 
+				// special case, don't touch CDATA elements
+	                replaceInElement(child, find, replace);
+	        } else if (child.nodeType==3) { 
+			// TEXT_NODE
+	            replaceInText(child, find, replace);	
+	        }
+	    }
+	};
+
+	Converter.prototype.replaceInText = function (text, find, replace) {
+    var match, matches = [];
 	
     while (match= find.exec(text.data))
-	
-		matches.push(match);
-		
+			matches.push(match);
     for (var i= matches.length; i-->0;) {
-	
-        match= matches[i];
-		
-        text.splitText(match.index);
-		
-        text.nextSibling.splitText(match[0].length);
-		
-        text.parentNode.replaceChild(replace(match), text.nextSibling);
-		
+	        match= matches[i];
+	        text.splitText(match.index);
+	        text.nextSibling.splitText(match[0].length);
+	        text.parentNode.replaceChild(replace(match), text.nextSibling);
 		}
-
     };
-//need to be put in a single function ASP !
 
-function fromMilesToKm(match){
-	var kilometers;
-	kilometers = match * 1.60934;
-	return kilometers.toFixed(2);
-};
-function fromKmtoMi(match){
-	var miles;
-	miles = match / 1.60934;
-	return miles.toFixed(2);
-};
-// function fromFeetToMeters(match){
-	// var meters;
-	// meters = match / 3.2808;
-	// return meters.toFixed(2);
-// };
-// function fromMetersToFeet(match){
-	// var feet;
-	// feet = match * 3.2808;
-	// return feet.toFixed(2);
-// };
-// function fromInchToCm(match){
-	// var cm;
-	// cm = match/0.39370;
-	// return cm.toFixed(2);
-// };
-// function fromCmToInch(match){
-	// var inch;
-	// inch = match * 0.39370;
-	// return inch.toFixed(2);
-// };
-chrome.storage.sync.get(["usrOp","urllist"], function(items) {
-	var url = window.location.host;
-	var t = items.urllist || [];
-	if (t.indexOf(url) == -1){
-		if(items.usrOp === 'imperial'){
-			//var kmMatch= /(\d+((\.|,)\d+)?)\s*(km|Km|Kilometer|Kilometers|kilometers)\b/g;
-			var kmMatch = /((\d{1,3}([,\s.']\d{3})*|\d+)([.,]\d+)?)\s*(km|Km|Kilometer|Kilometers|kilometers)\b/g;
-			replaceInElement(document.body, kmMatch, function(match) {
-				//var link= document.createElement('a');
-				var itmReplaced = document.createElement('b');
-				//itmReplaced.style.cssText = 'color: rgb(24, 100, 88);background-color: #eee;';
-				var value = match[2].replace(/[,\s.']/, "") + (match[4] !== undefined ? match[4].replace(/[,\s.']/, ".") : ".00");
-				itmReplaced.appendChild(document.createTextNode(fromKmtoMi(value)+' Miles '));
-				return itmReplaced;
-			});
-		}else if(items.usrOp === 'metric'){
-			//var miMatch= /(\d+((\.|,)\d+)?)\s*(mil|Mil|mile|Mile|miles|Miles)\b/g;
-			var miMatch = /((\d{1,3}([,\s.']\d{3})*|\d+)([.,]\d+)?)\s*(mil|Mil|mile|Mile|miles|Miles)\b/g;
-			replaceInElement(document.body, miMatch, function(match) {
-				//var link= document.createElement('a');
-				var itmReplaced = document.createElement('b');
-				//itmReplaced.style.cssText = 'color: rgb(24, 100, 88);background-color: #eee;';	
-				var value = match[2].replace(/[,\s.']/, "") + (match[4] !== undefined ? match[4].replace(/[,\s.']/, ".") : ".00");
-				itmReplaced.appendChild(document.createTextNode(fromMilesToKm(value)+' Kilometer '));
-				return itmReplaced;
-			});
+    Converter.prototype.fromMilesToKm = function(match){
+		var kilometers;
+		kilometers = match * 1.60934;
+		return kilometers.toFixed(2);
+	};
+
+	Converter.prototype.fromKmtoMi = function (match){
+		var miles;
+		miles = match / 1.60934;
+		return miles.toFixed(2);
+	};
+	
+	document.ondomcontentready = function (){
+		if(converter === 'undefined'){
+			new Converter();
 		}
-	}
-});
-//listen for the options.js for any user imput (eg. save changes) 
-chrome.runtime.onMessage.addListener(
-	function(request, sender,sendResponse) {
-	
-	if (request.applyScript == "restartWithScript" ){
-	
-		location.reload();
-	  }
-	  else {
-	  }
-});
+	};
+}();
--- unit converter/options.js
+++ unit converter/options.js
@@ -1,205 +1,189 @@
-Array.prototype.contains = function ( needle ) {
-	for (i in this) {
-		if (this[i] == needle) return true;
-	}
-	return false;
-}
-function parseUri (str) {
-	var	o   = parseUri.options,
-		m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
-		uri = {},
-		i   = 14;
++function(){
+	var ConverterOptions = function(){
+		this.parseUri.options = {
+			strictMode: false,
+			key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+			q  : {
+				name:   "queryKey",
+				parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+			},
+			parser: {
+				strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+				loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+			},
+			strictMode: true
+		};
 
-	while (i--) uri[o.key[i]] = m[i] || "";
-
-	uri[o.q.name] = {};
-	uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
-		if ($1) uri[o.q.name][$1] = $2;
-	});
-
-	return uri;
-};
-parseUri.options = {
-	strictMode: false,
-	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
-	q:   {
-		name:   "queryKey",
-		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
-	},
-	parser: {
-		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
-	}
+		this.init();
+	};
 	
-};
-parseUri.options.strictMode = true;
-
-function save_options() {
-	// Get a value saved in a form.
+	ConverterOptions.prototype.init = function(){
+		this.attachEvents();
+	};
 	
-	function getCheckedRadioValue(radioGroupName) {
-		   var rads = document.getElementsByName(radioGroupName),
-			   i;
-		   for (i=0; i < rads.length; i++)
-			  if (rads[i].checked)
-				  return rads[i].value;
-		   return undefined;
-		   }
-	var theValue = getCheckedRadioValue("system");
-	if (!theValue){
-	var status = document.getElementById('status');
-						status.textContent = 'Please select one of the above and well do the rest!';
+	ConverterOptions.prototype.attachEvents = function(){
+		document.addEventListener('DOMContentLoaded', restore_options());
+		document.addEventListener('DOMContentLoaded', function () {
+			document.getElementById('save').addEventListener('click',save_options);
+			document.getElementById('clrusrdat').addEventListener('click', clear_data);	
+			document.getElementById('like').addEventListener('change',connEcted);
+			document.getElementById('support_link').addEventListener('click', openSupportPage);
+		});
+	};
+
+	ConverterOptions.prototype.contains = function ( needle ) {
+		for (i in this) {
+			if (this[i] == needle) return true;
+		}
+		return false;
+	};
+
+	ConverterOptions.prototype.parseUri = function(str) {
+		var	o   = parseUri.options,
+			m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+			uri = {},
+			i   = 14;
+
+		while (i--) uri[o.key[i]] = m[i] || "";
+
+		uri[o.q.name] = {};
+		uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+			if ($1) uri[o.q.name][$1] = $2;
+		});
+
+		return uri;
+	};
+
+	ConverterOptions.prototype.save_options = function(){
+		var theValue = getCheckedRadioValue("system"),status = document.getElementById('status');;
+
+		if (!theValue){
+			status.textContent = 'Please select one of the above and well do the rest!';
+			setTimeout(function() {
+				status.textContent = '';
+			}, 3000);
+			return;
+		}
+
+		// Save it using the Chrome extension storage API.
+		chrome.storage.sync.set({'usrOp': theValue},  function() {
+		// Update status to let user know options were saved.
+			var status = document.getElementById('status');
+			status.textContent = 'Options saved.';
+			setTimeout(function() {
+			status.textContent = '';
+			}, 1750);
+			
+		});
+		if (theValue === "imperial"){
+		chrome.browserAction.setIcon({path:"images/mi.png"});
+		}
+		else if (theValue === "metric"){
+		chrome.browserAction.setIcon({path:"images/km.png"});
+		}
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {'usrOp': theValue, applyScript : "restartWithScript"}, function(response) {	
+			});
+		});
+	};
+
+	ConverterOptions.prototype.getCheckedRadioValue =  function(radioGroupName) {
+		var rads = document.getElementsByName(radioGroupName),i;
+
+		for (i=0; i < rads.length; i++)
+		  if (rads[i].checked)
+			  return rads[i].value;
+		return undefined;
+	};
+	
+	ConverterOptions.prototype.restore_options = function() {
+
+		chrome.storage.sync.get("usrOp", function(items) {
+			if(items.usrOp === 'imperial'){
+				document.holder.system[0].checked = true;
+				chrome.browserAction.setIcon({path:"images/mi.png"});
+			}else if(items.usrOp === 'metric'){
+				document.holder.system[1].checked = true;
+				chrome.browserAction.setIcon({path:"images/km.png"});
+			}
+			
+		});
+		chrome.tabs.getSelected(null, function(tab) {
+			var url = tab.url;
+			var res = parseUri(url);
+			url = res.host;		
+			chrome.storage.sync.get(["urllist"],function (obj){
+				var t = obj.urllist || [];
+				if (t.contains(url)) 
+				{
+					document.getElementById('like').checked = true;
+				}
+			});
+		});		
+	};
+	
+	ConverterOptions.prototype.clear_data = function() {
+	    chrome.storage.sync.remove(["urllist","usrOp"], function() {
+	        for(var i = 0; i < key.length; i++)
+	            key[i];
+	    });
+		if(document.getElementById('like').checked == true)
+		document.getElementById('like').checked = false;
+		document.holder.system[0].checked 		= false;
+		document.holder.system[1].checked		= false;
+		chrome.browserAction.setIcon({path:"images/16.png"});
+		var status = document.getElementById('status');
+		status.textContent = 'Options restored!';
+		setTimeout(function() {
+			status.textContent = '';
+		}, 3000);
+	};
+	
+	ConverterOptions.prototype.connEcted = function(){
+		chrome.tabs.query({
+			active: true,               
+			lastFocusedWindow: true
+		},function(array_of_Tabs) {
+			var tab = array_of_Tabs[0], url = tab.url, res = parseUri(url);
+			url = res.host;		
+			chrome.storage.sync.get(["urllist"],function (obj){
+				var t = obj.urllist || [], contains = t.indexOf(url);
+			
+				if (contains > -1){
+					t.splice(contains, 1);
+					chrome.storage.sync.set({'urllist': t}, function (){ 
+						var status = document.getElementById('status');
+						status.textContent = 'Removed from dont run list! '+ '(' + url +')';
+						status.style.color = 'green';
 						setTimeout(function() {
 						status.textContent = '';
 						}, 3000);
-	 return;
-	 }
-
-	// Save it using the Chrome extension storage API.
-	chrome.storage.sync.set({'usrOp': theValue},  function() {
-	// Update status to let user know options were saved.
-		var status = document.getElementById('status');
-		status.textContent = 'Options saved.';
-		setTimeout(function() {
-		status.textContent = '';
-		}, 1750);
-		
-	});
-	if (theValue === "imperial"){
-	chrome.browserAction.setIcon({path:"images/mi.png"});
-	}
-	else if (theValue === "metric"){
-	chrome.browserAction.setIcon({path:"images/km.png"});
-	}
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {'usrOp': theValue, applyScript : "restartWithScript"}, function(response) {	
-		});
-	});
-}
-
-//self explanatory restore options on popup acctivated based on user pref
-function restore_options() {
-
-	chrome.storage.sync.get("usrOp", function(items) {
-		if(items.usrOp === 'imperial'){
-			document.holder.system[0].checked = true;
-			chrome.browserAction.setIcon({path:"images/mi.png"});
-		}else if(items.usrOp === 'metric'){
-			document.holder.system[1].checked = true;
-			chrome.browserAction.setIcon({path:"images/km.png"});
-		}
-		
-	});
-	chrome.tabs.getSelected(null, function(tab) {
-		var url = tab.url;
-		var res = parseUri(url);
-		url = res.host;		
-		chrome.storage.sync.get(["urllist"],function (obj){
-			var t = obj.urllist || [];
-			if (t.contains(url)) 
-			{
-				document.getElementById('like').checked = true;
-			}
-		});
-	});		
-}
-
-function clear_data() {
-
-    chrome.storage.sync.remove(["urllist","usrOp"], function() {
-        for(var i = 0; i < key.length; i++)
-            key[i];
-    });
-	if(document.getElementById('like').checked == true)
-	document.getElementById('like').checked = false;
-	document.holder.system[0].checked 		= false;
-	document.holder.system[1].checked		= false;
-	chrome.browserAction.setIcon({path:"images/16.png"});
-	var status = document.getElementById('status');
-	status.textContent = 'Options restored!';
-	setTimeout(function() {
-	status.textContent = '';
-	}, 3000);
-}
-
-function connEcted(){
-	chrome.tabs.query({
-		active: true,               // Select active tabs
-		lastFocusedWindow: true     // In the current window
-	}, function(array_of_Tabs) {
-			// Since there can only be one active tab in one active window, 
-			//  the array has only one element
-			var tab = array_of_Tabs[0];
-			// Example:
-			var url = tab.url;
-		
-			var res = parseUri(url);
-			url = res.host;		
-			// ... do something with url variable
-		chrome.storage.sync.get(["urllist"],function (obj){
-		   //all my functions and code are wrapped inside here, the rest of the options      
-		   //returned inside g are either primitives (booleans) and I have one object. 
-
-			var t = obj.urllist || [];
-			
-			var contains = t.indexOf(url);
-			
-			if (contains > -1)
-			{
-				t.splice(contains, 1);
-				chrome.storage.sync.set({'urllist': t}, function (){ 
-					var status = document.getElementById('status');
-					status.textContent = 'Removed from dont run list! '+ '(' + url +')';
-					status.style.color = 'green';
-					setTimeout(function() {
-					status.textContent = '';
-					}, 3000);
-					
-				});
-			}
-			else
-			{
-				// to b
-				t.push(url); //Selector is a string
-				chrome.storage.sync.set({'urllist': t}, function (){ });   
-					var status = document.getElementById('status');
-					status.textContent = 'Added to dont run list!' + '(' + url + ')';
-					status.style.color = 'red';
-					setTimeout(function() {
-					status.textContent = '';
-					}, 3000);
-			}
-			
-			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-				chrome.tabs.sendMessage(tabs[0].id, {applyScript : 'restartWithScript'}, function(response) {	
+					});
+				}else{
+					t.push(url); //Selector is a string
+					chrome.storage.sync.set({'urllist': t}, function (){ });   
+						var status = document.getElementById('status');
+						status.textContent = 'Added to dont run list!' + '(' + url + ')';
+						status.style.color = 'red';
+						setTimeout(function() {
+						status.textContent = '';
+						}, 3000);
+				}
+				chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+					chrome.tabs.sendMessage(tabs[0].id, {applyScript : 'restartWithScript'}, function(response) {	
+					});
 				});
 			});
 		});
-	});
-};		
+	};
+	
+	ConverterOptions.prototype.openSupportPage =  function(){
+		chrome.tabs.create( { url: "http://jovcevski.mk/support"} );
+	};
 
-function openSupportPage(){
-
-	chrome.tabs.create( { url: "http://jovcevski.mk"} );
-
-}
-//add on load restore opt. needed for seatch of prev user choices in chrome.sync.get() .So fire away!
-document.addEventListener('DOMContentLoaded', restore_options());
-document.addEventListener('DOMContentLoaded', function () {
-	document.getElementById('save').addEventListener('click',save_options);
-	document.getElementById('clrusrdat').addEventListener('click', clear_data);	
-	document.getElementById('like').addEventListener('change',connEcted);
-	document.getElementById('support_link').addEventListener('click', openSupportPage);
-});
-//test for option change
-// chrome.storage.onChanged.addListener(function(changes, namespace) {
-	// for (key in changes) {
-	  // var storageChange = changes[key];
-	  // console.log('Storage key "%s" in namespace "%s" changed. ' +
-				  // 'Old value was "%s", new value is "%s".',
-				  // key,
-				  // namespace,
-				  // storageChange.oldValue,
-				  // storageChange.newValue);
-	// }
-// });
+	document.ondomcontentready = function (){
+		if(ConverterOptions === 'undefined'){
+			new ConverterOptions();
+		}
+	};
+}();
